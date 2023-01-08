e0ae7ed16c3f786d1c1951ba0a8029c99d2f24ba
Ryan Silva de Menezes <85131927+RyanSilva-176@users.noreply.github.com>
2023-01-08 17:19:00 -0300

    Added - Accessibility support

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -5,7 +5,8 @@ var heading_md = document.getElementsByClassName('heading-md')
 var text = document.getElementsByClassName('text')
 var logo = document.getElementsByClassName('logo-name')
 var icons = document.getElementsByClassName('fa-solid')
-
+let buttons = document.querySelector(".buttons");
+let btn = buttons.querySelectorAll(".btn");
 
 chk.addEventListener('change', () => {
   document.body.classList.toggle('light')
@@ -27,4 +28,15 @@ chk.addEventListener('change', () => {
   for (var x=0;x<icons.length;x++) {
     icons[x].classList.toggle('light')
   }
+  for (var x=0;x<btn.length;x++) {
+    btn[x].classList.toggle('light')
+  }
 })
+
+for (var i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    this.className += " active";
+  });
+}
